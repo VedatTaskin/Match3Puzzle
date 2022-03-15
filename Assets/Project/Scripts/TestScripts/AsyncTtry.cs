1ce79c874adea6e87caaeb16065dce20a83c56fb
VedatTaskin <taskinvedat86@gmail.com>
2022-03-15 14:43:28 +0300

    materials

---
+++ Assets/Project/Scripts/TestScripts/AsyncTtry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class AsyncTtry : MonoBehaviour
+{
+
+    //public IEnumerator RotateForSeconds (float duration)
+    //{
+    //    var end = Time.time + duration;
+    //    while (Time.time <end)
+    //    {
+    //        transform.Rotate( new Vector3(1,1) * Time.deltaTime * 150);
+    //        yield return null;
+    //    }
+    //}
+
+    public async Task RotateForSeconds(float duration)
+    {
+        var end = Time.time + duration;
+        while (Time.time < end)
+        {
+            transform.Rotate(new Vector3(1, 1) * Time.deltaTime * 150);
+            await Task.Yield();
+        }
+    }
+}
